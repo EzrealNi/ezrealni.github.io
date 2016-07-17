d3b9320f4188f01b0cc26bd390d1b13436559685
EzrealNi <863708821@qq.com>
2016-07-17 23:58:29 +0800

    Update calculator.js

--- project/calculator/js/calculator.js
+++ project/calculator/js/calculator.js
@@ -1,16 +1,25 @@
-$(function(){
-  setKeyWidth();
+$(function() {
+	setKeyWidth();
+	blindEvent();
 });
 
-function setKeyWidth(){
-  var calculatorWidth = $(".calculator").width(),
-      keyWidth = (calculatorWidth-5)/4,
-      zeroWidth = keyWidth*2+1;
-  $(".key:not(.screen,.zero)").width(keyWidth);
-  $(".key.zero").width(zeroWidth);
-  $(".key:not(.screen)").css("line-height",$(".key.zero").height()+"px");
+function setKeyWidth() {
+	var calculatorWidth = $(".calculator").width(),
+		keyWidth = (calculatorWidth - 5) / 4, zeroWidth = keyWidth * 2 + 1;
+	$(".key:not(.screen,.zero)").width(keyWidth);
+	$(".key.zero").width(zeroWidth);
+	$(".key:not(.screen)").css("line-height", $(".key.zero").height() + "px");
+}
+
+function blindEvent(){
+	$(".calculator > .row").on("click", ".calcula", function(){
+		$(".calcula.select").removeClass("select");
+		$(".calcula.bottom-select").removeClass("bottom-select");
+		$(this).addClass("select");
+		$(this).parent().prev().children(".calcula").addClass("bottom-select");
+	});
 }
 
 $(window).resize(function() {
-  setKeyWidth();
+	setKeyWidth();
 });
