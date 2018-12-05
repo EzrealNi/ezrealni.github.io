eb862398dba0f743223a5c8e1a6d4553f71d0d0b
mixin <mixinsoft@163.com>
2018-12-05 17:37:32 +0800

    Update index.js

--- project/xss/index.js
+++ project/xss/index.js
@@ -1,3 +1,12 @@
 document.querySelector('#app-1 .content img').style.filter = 'blur(5px)';
 
 // alert(document.cookie);
+
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 30);
+    document.cookie = name + '=' + escape(value) + ';expires=' + exp.toGMTString();
+}
+
+setCookie('unionID', '123456');
